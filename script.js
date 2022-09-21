1965bb03b85b7aacb328e6aff03ae27ab1f48726
Tarbiya131 <79188735+Tarbiya131@users.noreply.github.com>
2022-09-21 22:52:13 +0600

    udp

--- script.js
+++ script.js
@@ -1,54 +1,34 @@
-let arr = [5, 67, 142, 23, 1, 456, 86, 136, 234, 12];
-let min = Math.min(...arr);
-console.log(min);
-let max = Math.max(...arr);
-console.log(max);
-
-let arr1= [];
-for(let i = 0; i < 5; i++){
-    let a = +prompt("Введите число");
-    array.push(a);
+let x = "";
+for ( let i = 0; i < 8; i++){
+  for ( let j = 0; j < 8; j++){
+    if((i + j) % 2 == 0){
+      x += " ";
+    }else
+      x += "#"
+    
+  }
+  x += "\n";
 }
-console.log(arr1);
-
-let c = 0;
-for(let i = 0; i < arr1.length; i++){
-    c += array[i]
-}
-console.log(c);
-
-let d = c / arr1.length;
-console.log(d);
+console.log(x);
 
 
-let arr_1 = [254, 5, 56, 65, 143, 42, 3];
-let arr_2 = [15, 6, 56, 63, 143, 142, 13];
-for ( let i = 0; i < arr_1.length; i++){
-    let arr =[]
-    for( let j = i; j < i +1; j++){
-        if( arr_1[i] == arrTwo[j]){
-            arr.push("=")
-        }else if( arr_1[i] < arrTwo[j]){
-            arr.push("<")
-        }else if( arr_1[i] > arrTwo[j]){
-            arr.push(">")
-        } 
-    }
-    console.log(arr);
+
+for( let i = 1; i < 100 + 1; i ++){
+  if ( i % 3 == 0 && i % 5 != 0){
+    console.log((i) + " Fizz")
+  }else if ( i % 3 != 0 && i % 5 ==0){
+    console.log((i) + " Buzz")
+  }else console.log((i) + " FizzBuzz")
 }
 
-
-
-
-
-
-let arr2 = [254, 5, 56, 65, 143, 42, 3, 241, 176, 10];
-let newArr = [];
-for (let i = 0; i < arr2.length; i++){
-    if(arr2[i]%2 == 0){
-        newArr.push(arr2[i])
-    }
-   
+function str(){
+  let y = prompt("введите слова");
+  y = y.toLowerCase().replace(/[^a-z0-9]+/g,'');
+  let z = y.split("");
+  z.reverse();
+  z = z.join("");
+  if( y == z){
+    console.log("Палиндром")
+  }else console.log("Не палиндром")
 }
-console.log(arr2)
-console.log(newArr)
+str()
